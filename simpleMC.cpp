c18c8f925a2738461bc818f89604e42ba25b3715
sauvm <mathsauvageau@icloud.com>
2018-12-31 12:13:47 -0500

    Changed the file to use the payoff class

--- simpleMC.cpp
+++ simpleMC.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <cmath>
 #include <random>
-#include "lib/Random1.h"
+#include "include/Random1.h"
+#include "include/PayOff1.h"
+#include "include/MinMax.h"
 using namespace std;
 
-double SimpleMonteCarlo(double Expiry,
-                        double Strike,
+double SimpleMonteCarlo(const PayOff& thePayOff,
+                        double Expiry,
                         double Spot,
                         double Vol,
                         double r,
@@ -18,17 +20,17 @@ double SimpleMonteCarlo(double Expiry,
     double movedSpot = Spot*exp(r*Expiry + itoCorrection);
     double thisSpot;
     double runningSum = 0;
-
-    std::default_random_engine generator (123544);
-    std::normal_distribution<double> distribution(0.0, 1.0);
+    
+    //this is in the std library
+    default_random_engine generator (123544);
+    normal_distribution<double> distribution(0.0, 1.0);
 
     for (unsigned long i = 0; i < NumberOfPaths; i++)
     {
         double thisGaussian = distribution(generator);
         thisSpot = movedSpot * exp(rootVariance*thisGaussian);
-        double thisPayoff = thisSpot - Strike;
-        thisPayoff = thisPayoff > 0 ? thisPayoff : 0;
-        runningSum += thisPayoff;
+        double thisPayOff = thePayOff(thisSpot);
+        runningSum += thisPayOff;
     }
 
     double mean = runningSum / NumberOfPaths;
@@ -40,14 +42,22 @@ double SimpleMonteCarlo(double Expiry,
 
 int main() {
 
-    double toto = GetOneGaussianByBoxMuller();
-    cout<<toto;
+    PayOff callPayOff(120, PayOff::call);
+    PayOff putPayOff(120, PayOff::put);
 
-    double result = SimpleMonteCarlo(1,
-                                     120,
+    double Cresult = SimpleMonteCarlo(callPayOff,
+                                     1,
                                      100,
                                      0.4,
                                      0.03,
                                      5000000);
-    cout << "The price is " << result;
+    cout << "The price of a call is " << Cresult << "\n";
+
+    double Presult = SimpleMonteCarlo(putPayOff,
+                                     1,
+                                     100,
+                                     0.4,
+                                     0.03,
+                                     5000000);
+    cout << "The price of a put is " << Presult;
 };
